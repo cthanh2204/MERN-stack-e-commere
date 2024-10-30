34521b235cc4aaf051a65b56d36f57c8e933b031
cthanh2204 <84348639+cthanh2204@users.noreply.github.com>
2024-10-30 10:52:41 +0700

    update pagination

--- client/E-commerce/src/components/Pagination.jsx
+++ client/E-commerce/src/components/Pagination.jsx
@@ -2,19 +2,33 @@ import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const Pagination = ({ pages, page, isAdmin = false, link }) => {
+  const paginationSize = 5;
+  const startPage = Math.floor(page / 5) * paginationSize + 1;
+  const endPage = Math.min(startPage + paginationSize - 1, pages); // endPage = 10
+  console.log({ startPage });
+  console.log({ endPage });
   return (
     pages > 1 && (
       <div className="join w-full justify-center align-center">
-        {[...Array(pages).keys()].map((x) => (
-          <Link
-            key={x + 1}
-            to={!isAdmin ? `/page/${x + 1}` : `${link}/${x + 1}`}>
-            <button
-              className={`join-item btn ${x + 1 === page ? "btn-active" : ""}`}>
-              {x + 1}
-            </button>
+        {startPage > 5 ? (
+          <Link to={`/page/${page - 1}`}>
+            <button className="join-item btn">«</button>
           </Link>
-        ))}
+        ) : null}
+        {[...Array(endPage - startPage + 1).keys()].map((x) => {
+          return (
+            <Link
+              key={startPage + x}
+              to={!isAdmin ? `/page/${startPage + x}` : `${link}/${x + 1}`}>
+              <button
+                className={`join-item btn ${
+                  startPage + x === page ? "btn-active" : ""
+                }`}>
+                {startPage + x}
+              </button>
+            </Link>
+          );
+        })}
       </div>
     )
   );
