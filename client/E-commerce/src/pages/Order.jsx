0a7b8650b8e22ac3c7401668650c4c0725709356
cthanh2204 <84348639+cthanh2204@users.noreply.github.com>
2024-12-14 19:40:13 +0700

    update admin order list and package

--- client/E-commerce/src/pages/Order.jsx
+++ client/E-commerce/src/pages/Order.jsx
@@ -170,13 +170,7 @@ const Order = () => {
                 <b>Total: </b>
                 <p>${order.totalPrice}</p>
               </div>
-              {!order?.isPaid ? (
-                <button
-                  className="btn btn-neutral w-full"
-                  onClick={successPaymentHandler}>
-                  Order
-                </button>
-              ) : null}
+
               {userInfo?.isAdmin && order.isPaid && !order.isDelivered && (
                 <button
                   className="btn btn-neutral w-full"
@@ -184,6 +178,29 @@ const Order = () => {
                   Mark as Delivered
                 </button>
               )}
+              {userInfo?.isAdmin && !order?.isPaid && !order.isDelivered ? (
+                <div role="alert" className="alert alert-warning">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 shrink-0 stroke-current"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                    />
+                  </svg>
+                  <span>Warning: The User has not pay the product!</span>
+                </div>
+              ) : !order?.isPaid ? (
+                <button
+                  className={`btn-neutral w-full btn`}
+                  onClick={successPaymentHandler}>
+                  Order
+                </button>
+              ) : null}
             </div>
           </div>
         </div>
